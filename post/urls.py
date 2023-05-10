211537e40195248abb92886504c97c8aea6c45a0
Айсулу Токтогулова <aisulutoktogulova@MacBook-Air-Ajsulu.local>
2023-05-10 22:26:22 +0600

    first

---
+++ post/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import ProductView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('product', ProductView)
+
+
+urlpatterns =[
+    path('', include(router.urls)),
+]
